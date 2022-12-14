0ebb9704bb04961e5f613ca7cf6ab78ae60c18a0
Nikita Krasnov <n.krasnov@nkrasnov.local>
2022-12-15 02:36:24 +0300

    feat: add loading state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useRef} from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import './App.css'
 function App() {
-  const { unityProvider } = useUnityContext({
+    const f = useRef();
+  const { unityProvider, loadingProgression, isLoaded  } = useUnityContext({
     loaderUrl: "buildunity/BUILD.loader.js",
     dataUrl: "buildunity/BUILD.data",
     frameworkUrl: "buildunity/BUILD.framework.js",
@@ -10,7 +11,12 @@ function App() {
   });
   return (
       <div className="App">
-        <Unity unityProvider={unityProvider} style={{ width: 600, height: 600 }} />
+          <>
+              {!isLoaded && (
+                  <p className="loadingText">Loading Application... {Math.round(loadingProgression * 100)}%</p>
+              )}
+              <Unity ref={f} unityProvider={unityProvider} style={{ width: 600, height: 600 }} />
+          </>
       </div>
   );
 }
